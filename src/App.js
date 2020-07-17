d80c6285ebd47dab5a7aad0df1e5cd2151d08565
lsamano <leizl.samano@gmail.com>
2020-07-17 14:19:10 -0400

    updated Home, static only

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        Header
-      </header>
       <main>
         <Switch>
           <Route component={Home} />
         </Switch>
       </main>
+      <footer>
+        <Tooltip title="LinkedIn" arrow>
+          <IconButton className="fab fa-linkedin" fontSize="large"/>
+        </Tooltip>
+        <Tooltip title="GitHub" arrow>
+        <IconButton className="fab fa-github" fontSize="large"/>
+        </Tooltip>
+        <Tooltip title="Medium" arrow>
+        <IconButton className="fab fa-medium" fontSize="large"/>
+        </Tooltip>
+      </footer>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
+import Icon from '@material-ui/core/Icon';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+// import PersonIcon from '@material-ui/icons/Person';
+// import CodeIcon from '@material-ui/icons/Code';
 // import LinkedInIcon from '@material-ui/icons/LinkedIn';
 // import GitHubIcon from '@material-ui/icons/GitHub';
 
 
 const Home = () => {
   return (
-    <div>
+    <Container component={Paper} className="main-div">
+    <img src="/images/avatar.jpg" style={{"borderRadius":"50%"}}/>
     <Typography variant="h1" component="h2" gutterBottom>
       Leizl Samano
     </Typography>
-    <Typography variant="h4" gutterBottom>
-      Software Engineer
+    <Typography variant="h4" color="primary" gutterBottom>
+      Full Stack Software Engineer
     </Typography>
-    <Tooltip title="LinkedIn" arrow>
-      <IconButton className="fab fa-linkedin" fontSize="large"/>
-    </Tooltip>
-    <Tooltip title="GitHub" arrow>
-    <IconButton className="fab fa-github" fontSize="large"/>
-    </Tooltip>
-    <Tooltip title="Medium" arrow>
-    <IconButton className="fab fa-medium" fontSize="large"/>
-    </Tooltip>
-    </div>
+
+    <ButtonGroup size="large" color="secondary" aria-label="large outlined button group">
+      <Button startIcon={<Icon className="fas fa-user-circle"/>}>About Me</Button>
+      <Button startIcon={<Icon className="fas fa-terminal"/>}>Projects</Button>
+      <Button startIcon={<Icon className="fas fa-file-alt"/>}>Resume</Button>
+      <Button startIcon={<Icon className="fas fa-pen-fancy"/>}>Blog</Button>
+    </ButtonGroup>
+    </Container>
   )
 }
 
