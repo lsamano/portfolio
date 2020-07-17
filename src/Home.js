4f091b48592e51259d7b13fb3f395e066e898ae8
lsamano <leizl.samano@gmail.com>
2020-07-17 10:17:41 -0400

    home page wip

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 const Home = () => {
   return (
     <div>
-    Hello Home
+    <Typography variant="h1" component="h2" gutterBottom>
+      Leizl Samano
+    </Typography>
+    <Typography variant="h4" gutterBottom>
+      Software Engineer
+    </Typography>
+    <Icon className="fab fa-linkedin" fontSize="large"/>
+    <Icon className="fab fa-github" fontSize="large"/>
+    <Icon className="fab fa-medium" fontSize="large"/>
     </div>
   )
 }
