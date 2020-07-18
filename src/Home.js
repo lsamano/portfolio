04ee5e63874ddc474151439a3dc3ab3fbdc3566f
lsamano <leizl.samano@gmail.com>
2020-07-18 18:26:04 -0400

    added alt text

--- src/Home.js
+++ src/Home.js
@@ -5,31 +5,41 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
-// import PersonIcon from '@material-ui/icons/Person';
-// import CodeIcon from '@material-ui/icons/Code';
-// import LinkedInIcon from '@material-ui/icons/LinkedIn';
-// import GitHubIcon from '@material-ui/icons/GitHub';
-
+// import { DiHtml5, DiCss3, DiRor, DiRuby, DiJavascript1, DiReact, DiNodejs, DiPostgresql, DiMongodb } from 'react-icons/di';
 
 const Home = () => {
   return (
     <Container component={Paper} className="main-div">
-    <img src="/images/avatar.jpg" style={{"borderRadius":"50%"}}/>
-    <Typography variant="h1" component="h2" gutterBottom>
-      Leizl Samano
-    </Typography>
-    <Typography variant="h4" color="primary" gutterBottom>
-      Full Stack Software Engineer
-    </Typography>
-
-    <ButtonGroup size="large" color="secondary" aria-label="large outlined button group">
-      <Button startIcon={<Icon className="fas fa-user-circle"/>}>About Me</Button>
-      <Button startIcon={<Icon className="fas fa-terminal"/>}>Projects</Button>
-      <Button startIcon={<Icon className="fas fa-file-alt"/>}>Resume</Button>
-      <Button startIcon={<Icon className="fas fa-pen-fancy"/>}>Blog</Button>
-    </ButtonGroup>
+      <img src="/images/avatar.jpg" alt="Leizl Samano" style={{"borderRadius":"50%"}}/>
+      <Typography variant="h1" component="h2" gutterBottom>
+        Leizl Samano
+      </Typography>
+      <Typography variant="h4" color="primary" gutterBottom>
+        Full Stack Software Engineer
+      </Typography>
+      <ButtonGroup size="large" color="secondary" aria-label="large outlined button group">
+        <Button startIcon={<Icon className="fas fa-user-circle"/>}>About Me</Button>
+        <Button startIcon={<Icon className="fas fa-terminal"/>}>Projects</Button>
+        <Button startIcon={<Icon className="fas fa-file-alt"/>}>Resume</Button>
+        <Button startIcon={<Icon className="fas fa-pen-fancy"/>}>Blog</Button>
+      </ButtonGroup>
     </Container>
   )
 }
+
+// <div className="dev-icons">
+//   <DiHtml5 size={32}/>
+//   <DiCss3 size={32}/>
+//   <br/>
+//   <DiJavascript1 size={32}/>
+//   <DiNodejs size={32}/>
+//   <DiReact size={32}/>
+//   <br/>
+//   <DiRuby size={32}/>
+//   <DiRor size={32}/>
+//   <br/>
+//   <DiPostgresql size={32}/>
+//   <DiMongodb size={32}/>
+// </div>
 
 export default Home;
