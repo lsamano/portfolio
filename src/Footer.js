4e7ec16ce16db6ef2d595f17e4956832bf5da26c
lsamano <leizl.samano@gmail.com>
2020-07-19 10:59:15 -0400

    links open in new tab

--- src/Footer.js
+++ src/Footer.js
@@ -1,29 +1,25 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 
-const Footer = () => {
-
-  return (
-    <>
+const Footer = () => (
+  <>
     <Tooltip title="LinkedIn" arrow>
-      <a href="https://www.linkedin.com/in/leizlsamano/">
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/leizlsamano/">
         <IconButton className="fab fa-linkedin" fontSize="large"/>
       </a>
     </Tooltip>
     <Tooltip title="GitHub" arrow>
-      <a href="https://github.com/lsamano">
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/lsamano">
         <IconButton className="fab fa-github" fontSize="large"/>
       </a>
     </Tooltip>
     <Tooltip title="Medium" arrow>
-      <a href="https://medium.com/@leizl.samano">
+      <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@leizl.samano">
         <IconButton className="fab fa-medium" fontSize="large"/>
       </a>
     </Tooltip>
-    </>
-  );
-}
+  </>
+)
 
 export default Footer;
