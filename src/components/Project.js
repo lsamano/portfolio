5ba38e2140b9d6cefdbabb6d3e8dc3dbb84b0492
lsamano <leizl.samano@gmail.com>
2020-07-24 11:09:34 -0400

    adding projects wip

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { useTheme } from '@material-ui/core/styles';
+
 // import PropTypes from 'prop-types';
 
 const Project = ({}) => {
+  const theme = useTheme();
+  console.log(theme);
+  const [ imageUrl, setImageUrl ] = useState("")
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/lsamano/tetris-v2/contents/images")
+    .then(res => res.json())
+    .then(data => setImageUrl(data[1].download_url))
+  }, [])
+
   return (
-    <div>One Project</div>
+    <Card>
+  <CardContent>
+    <Typography variant="h5" component="h2">
+      Tetris v2
+    </Typography>
+    <Typography color="textSecondary" gutterBottom>
+      Challenge your friends in Tetris
+    </Typography>
+    <Typography color="textPrimary">
+      <span style={{backgroundColor: theme.palette.primary.dark, padding:"5px", margin:"5px"}}>
+        JavaScript
+      </span>
+      <span style={{backgroundColor: theme.palette.primary.dark, padding:"5px", margin:"5px"}}>
+        Express
+      </span>
+      <span style={{backgroundColor: theme.palette.primary.dark, padding:"5px", margin:"5px"}}>
+        WebSockets
+      </span>
+    </Typography>
+    <CardMedia
+      component="img"
+      src={imageUrl}
+      alt="project image"
+      style={{width:"40vw", marginTop:"10px"}}
+      />
+  </CardContent>
+</Card>
+
   );
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Project from '../components/Project';
 import Typography from '@material-ui/core/Typography';
 
 const Projects = ({}) => {
-  const [ imageUrl, setImageUrl ] = useState("")
-  useEffect(() => {
-    fetch("https://api.github.com/repos/lsamano/tetris-v2/contents/images")
-    .then(res => res.json())
-    .then(data => setImageUrl(data[1].download_url))
-  }, [])
 
   return (
     <>
-    <img src={imageUrl} alt="project image" style={{height: "50%", width: "50%"}}/>
     <Typography variant="h2" component="h2" gutterBottom>
       Projects
     </Typography>
+
     <Typography variant="h6" component="h6" gutterBottom>
       Personal Projects
     </Typography>
+    <div style={{display: "flex"}}>
+      <Project />
+    </div>
+
     <Typography variant="h6" component="h6" gutterBottom>
       Apps for Learning
     </Typography>
