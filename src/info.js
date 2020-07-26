c778f1f55e0fdef3bb02a0c45aa1bec92ae57768
lsamano <leizl.samano@gmail.com>
2020-07-26 19:48:57 -0400

    projects info added

--- src/info.js
+++ src/info.js
@@ -2,9 +2,33 @@ export const projects = [
   {
     title: "Tetris v2",
     subtitle: "Challenge your friends in Tetris",
-    img_url: "https://raw.githubusercontent.com/lsamano/tetris-v2/master/images/img.png",
+    img_url: "https://raw.githubusercontent.com/lsamano/tetris-v2/master/images/preview.png",
     tags: [
       "JavaScript", "Express", "WebSockets"
+    ]
+  },
+  {
+    title: "Tourn",
+    subtitle: "Assemble an eSports team, win some tournaments",
+    img_url: "https://raw.githubusercontent.com/lsamano/tourn-client/master/images/preview.png",
+    tags: [
+      "JavaScript", "React.js", "Redux", "Ruby on Rails", "PostgreSQL", "JSON Web Tokens"
+    ]
+  },
+  {
+    title: "OhSnap",
+    subtitle: "Share photos, like, comment, and follow",
+    img_url: "https://raw.githubusercontent.com/lsamano/OhSnap/master/images/preview.png",
+    tags: [
+      "Ruby", "Ruby on Rails", "PostgreSQL"
+    ]
+  },
+  {
+    title: "Moon Harvest",
+    subtitle: "Enjoy a farming life in space",
+    img_url: "https://raw.githubusercontent.com/lsamano/Moon-Harvest-CLI-Game-v2/master/images/preview.png",
+    tags: [
+      "Ruby"
     ]
   }
 ]
