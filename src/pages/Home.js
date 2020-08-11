2bc1c72b17bce79088f662d932ef244d584c2b68
lsamano <leizl.samano@gmail.com>
2020-08-11 16:14:01 -0400

    cleaned up code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Icon from '@material-ui/core/Icon';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-// import { DiHtml5, DiCss3, DiRor, DiRuby, DiJavascript1, DiReact, DiNodejs, DiPostgresql, DiMongodb } from 'react-icons/di';
 
 const Home = () => {
   return (
@@ -29,20 +28,5 @@ const Home = () => {
     </>
   )
 }
-
-// <div className="dev-icons">
-//   <DiHtml5 size={32}/>
-//   <DiCss3 size={32}/>
-//   <br/>
-//   <DiJavascript1 size={32}/>
-//   <DiNodejs size={32}/>
-//   <DiReact size={32}/>
-//   <br/>
-//   <DiRuby size={32}/>
-//   <DiRor size={32}/>
-//   <br/>
-//   <DiPostgresql size={32}/>
-//   <DiMongodb size={32}/>
-// </div>
 
 export default Home;
