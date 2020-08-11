ef76bc4758205dbff7b951289fc63a4454576708
lsamano <leizl.samano@gmail.com>
2020-08-11 16:12:16 -0400

    Updates

---
+++ precache-manifest.91d8cfb1f6fbd65c5c7e9a9a278bc697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf01180b851f612e2bbbf410387a28d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "783f60965fc7fa12e092",
+    "url": "/static/css/main.eda75ff7.chunk.css"
+  },
+  {
+    "revision": "a848b5fb8b4aca636315",
+    "url": "/static/js/2.bff2b905.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.bff2b905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783f60965fc7fa12e092",
+    "url": "/static/js/main.f3a5bf1a.chunk.js"
+  },
+  {
+    "revision": "0a9c4f824e57e0278dc0",
+    "url": "/static/js/runtime-main.b0c4a842.js"
+  },
+  {
+    "revision": "3d6e4236057f2fd62b61bc0882cc9637",
+    "url": "/static/media/avatar.3d6e4236.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.31647bf8a4cf421a0470fc39b68292c0.js"
+  "/precache-manifest.91d8cfb1f6fbd65c5c7e9a9a278bc697.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
