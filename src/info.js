777d858a995c6e7789c279fea3ec127fa673ec56
lsamano <leizl.samano@gmail.com>
2020-08-14 11:11:21 -0400

    info updated with repos and demos

--- src/info.js
+++ src/info.js
@@ -8,6 +8,8 @@ export const projects = [
     title: "Tetris v2",
     subtitle: "Challenge your friends in Tetris",
     img_url: "https://raw.githubusercontent.com/lsamano/tetris-v2/master/images/preview.png",
+    github: "https://github.com/lsamano/tetris-v2",
+    demo: "http://tetris-v2.herokuapp.com/",
     tags: [
       "JavaScript", "Express", "WebSockets"
     ]
@@ -16,6 +18,8 @@ export const projects = [
     title: "Tourn",
     subtitle: "Assemble an eSports team, win some tournaments",
     img_url: "https://raw.githubusercontent.com/lsamano/tourn-client/master/images/preview.png",
+    github: "https://github.com/lsamano/tourn-client",
+    demo: "https://tourn-tournaments.herokuapp.com/",
     tags: [
       "JavaScript", "React", "Redux", "Ruby on Rails", "PostgreSQL"
     ]
@@ -24,6 +28,8 @@ export const projects = [
     title: "OhSnap",
     subtitle: "Share photos, like, comment, and follow",
     img_url: "https://raw.githubusercontent.com/lsamano/OhSnap/master/images/preview.png",
+    github: "https://github.com/lsamano/OhSnap",
+    demo: "https://ohsnap-app.herokuapp.com/",
     tags: [
       "Ruby", "Ruby on Rails", "PostgreSQL"
     ]
@@ -32,6 +38,8 @@ export const projects = [
     title: "Moon Harvest",
     subtitle: "Enjoy a farming life in space",
     img_url: "https://raw.githubusercontent.com/lsamano/Moon-Harvest-CLI-Game-v2/master/images/preview.png",
+    github: "https://github.com/lsamano/Moon-Harvest-CLI-Game-v2",
+    demo: "",
     tags: [
       "Ruby"
     ]
