8cca3a1414c5f6c7ffdad53a462bfffec26bdfc5
lsamano <leizl.samano@gmail.com>
2020-08-16 10:43:13 -0400

    changed content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <Typography variant="h2" component="h2" gutterBottom>
-        Leizl Samano
+        Hi, I'm Leizl.
       </Typography>
       <Typography variant="h4" gutterBottom>
-        Full Stack Software Engineer
+        Full Stack Software Engineer.
       </Typography>
-      <ButtonGroup
-        orientation="vertical"
-        size="large"
-        color="secondary"
-        aria-label="vertical contained secondary button group"
-        variant="text">
-        <Button startIcon={<Icon className="fas fa-user-circle"/>} component={Link} to="/about">About Me</Button>
-        <Button startIcon={<Icon className="fas fa-terminal"/>} component={Link} to="/projects">Projects</Button>
-        <Button startIcon={<Icon className="fas fa-file-alt"/>} component={Link} to="/resume">Resume</Button>
-        <Button startIcon={<Icon className="fas fa-pen-fancy"/>} component={Link} to="/blog">Blog</Button>
-      </ButtonGroup>
-    </>
+      <Typography variant="body1" gutterBottom>
+        Welcome to my site. This site was made using React.js and Material-UI for styling.
+        <br/> To continue exploring, please scroll down and enjoy.
+      </Typography>
+      <br/>
+      <Button className="long-button" size="large">
+        <i className="fas fa-angle-double-down fa-2x"></i><span style={{fontSize:"2em", paddingLeft:"10px"}}>Keep Going...</span>
+      </Button>
+
+    </div>
   )
 }
 
