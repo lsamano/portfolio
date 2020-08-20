f95ed9b2498b5386e3672587b5597e25d553cbb3
lsamano <leizl.samano@gmail.com>
2020-08-20 10:58:06 -0400

    blog wip

--- src/info.js
+++ src/info.js
@@ -45,3 +45,48 @@ export const projects = {
       ]
     }
 }
+
+export const blog = [
+  {
+    id: 1,
+    title: "How to Use .each_with_object in Ruby",
+    subtitle: "A quick overview of the method .each_with_object in Ruby.",
+    url: "https://medium.com/@leizl.samano/how-to-use-each-with-object-in-ruby-dccca07bfa18?source=friends_link&sk=d8fee2930423d9a47a594381e8a3b2e3",
+  },
+  {
+    id: 2,
+    title: "Coding Mentality — Be an Athlete and a Detective to Eliminate Every Error",
+    subtitle: "What do athletes and detectives have in common with programmers? It all comes down to mental processes.",
+    url: "https://medium.com/@leizl.samano/coding-mentality-be-an-athlete-and-a-detective-to-eliminate-every-error-ddef88838cf2?source=friends_link&sk=b74614f90c8df8b1f43418c9b0561090",
+  },
+  {
+    id: 3,
+    title: "How to Make a Weather App Using React",
+    subtitle: "Looking for a quick and clean React project to whip up? Try making this weather app!",
+    url: "https://medium.com/@leizl.samano/how-to-make-a-weather-app-using-react-403c88252deb?source=friends_link&sk=49f59f70518898763eaecbdd1f0f99e3",
+  },
+  {
+    id: 4,
+    title: "Using JWT in Your React+Redux App for Authorization",
+    subtitle: "An explanation and guide for how to handle JWT and Authentication in the frontend of your web app",
+    url: "https://levelup.gitconnected.com/using-jwt-in-your-react-redux-app-for-authorization-d31be51a50d2?source=friends_link&sk=e91837760476a3ac580ce367b45f57d7",
+  },
+  {
+    id: 5,
+    title: "Getting Started with React Hooks: Giving State and Lifecycle Methods to Functional Components",
+    subtitle: "An introduction to React Hooks, specifically useState and useEffect and how to implement them.",
+    url: "https://medium.com/swlh/getting-started-with-react-hooks-giving-state-and-lifecycle-methods-to-functional-components-1c9ac668ca73?source=friends_link&sk=d28b452be82769c7197cfccccc94edd0",
+  },
+  {
+    id: 6,
+    title: "How To Create a Follow Feature in Rails by Aliasing Associations",
+    subtitle: "Use macros to create association methods",
+    url: "https://medium.com/better-programming/how-to-create-a-follow-feature-in-rails-by-aliasing-associations-30d63edee284?source=friends_link&sk=71b47a775d4ee2bcb8ec6b4817a9f6de",
+  },
+  {
+    id: 7,
+    title: "Material-UI: How to Implement Dark Mode and Edit Theme Colors",
+    subtitle: "Customizing Material-UI can be a bit confusing for first-time users. Let’s explore how to edit the theme.",
+    url: "https://levelup.gitconnected.com/material-ui-how-to-implement-dark-mode-and-edit-theme-colors-effcfa0893b9?source=friends_link&sk=5403998930e735fe1173f962f17053be",
+  },
+]
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,13 +1,41 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { blog } from '../info';
 
 const Blog = ({ myRef }) => {
+  const formatBlogs = () => {
+    return blog.map(story => (
+      <Card style={{width: "30%", margin: "10px"}}>
+        <CardActionArea style={{height: "100%"}}>
+          <CardContent style={{height: "100%"}}>
+            <Typography style={{fontSize: 18}}>
+              {story.title}
+            </Typography>
+            <Typography color="textSecondary">
+              {story.subtitle}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    ))
+  }
+
   return (
     <section className="blog section" id="blog" ref={myRef}>
-    <Typography variant="h2" component="h2" align="center">
-    Blogs
-    </Typography>
+      <Container>
+        <Typography variant="h2" component="h2" align="center">
+          Blogs
+        </Typography>
+        <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-between"}}>
+          { formatBlogs() }
+        </div>
+      </Container>
   </section>
   );
 }
