a9ceb8cf9aef0b279d37cc9c1ec9393dc799de8e
lsamano <leizl.samano@gmail.com>
2020-08-25 17:05:00 -0400

    refactored

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,28 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import { useTheme } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 
-const ProjectCard = ({ title, subtitle, img_url, tags, shorthand }) => {
-  const theme = useTheme();
-
-  const style = {
+const useStyles = makeStyles(theme => ({
+  span: {
     backgroundColor: theme.palette.secondary.main,
     color: theme.palette.secondary.contrastText,
     padding: "5px",
     margin: "5px"
+  },
+  cardMedia: {
+    width:"100%"
+  },
+  tagContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center"
   }
+}));
+
+const ProjectCard = ({ title, subtitle, img_url, tags, shorthand }) => {
+  const { span, cardMedia, tagContainer } = useStyles();
 
   const renderTags = () => (
     tags.map((tag, index) => (
       <span
         key={index}
-        style={style}>
+        className={span}>
         {tag}
       </span>
     ))
@@ -30,27 +39,26 @@ const ProjectCard = ({ title, subtitle, img_url, tags, shorthand }) => {
 
   return (
     <Card style={{width: "45%", margin: "20px"}}>
-    <CardActionArea component={Link} to={`/projects/${shorthand}`}>
-    <CardMedia
-    component="img"
-    src={img_url}
-    alt="project image"
-    style={{width:"100%"}}
-    />
-  <CardContent>
-    <Typography variant="h5" component="h2">
-      {title}
-    </Typography>
-    <Typography color="textSecondary" gutterBottom>
-      {subtitle}
-    </Typography>
-        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      <CardActionArea>
+        <CardMedia
+        component="img"
+        src={img_url}
+        alt="project image"
+        className={cardMedia}
+        />
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {title}
+        </Typography>
+        <Typography color="textSecondary" gutterBottom>
+          {subtitle}
+        </Typography>
+        <div className={tagContainer}>
           {renderTags()}
         </div>
-  </CardContent>
-  </CardActionArea>
-</Card>
-
+      </CardContent>
+    </CardActionArea>
+  </Card>
   );
 }
 
