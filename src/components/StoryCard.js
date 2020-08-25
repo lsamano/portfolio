c8b05c6c6fe51c99c42a16c6e574c39ed31c2d1a
lsamano <leizl.samano@gmail.com>
2020-08-25 17:04:48 -0400

    added images to blogs

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -19,11 +20,12 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const StoryCard = ({ story: { title, subtitle, url } }) => {
+const StoryCard = ({ story: { title, subtitle, url, img_url } }) => {
   const { root, actionContent, titleFont } = useStyles();
   return (
     <Card className={root}>
       <CardActionArea className={actionContent} href={url}>
+        {img_url ? <CardMedia component="img" src={img_url} alt={title} /> : null}
         <CardContent className={actionContent}>
           <Typography className={titleFont}>
           {title}
