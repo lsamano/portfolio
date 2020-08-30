120243eaf3b616a25f2c70f46c29fe241c0d8a4e
lsamano <leizl.samano@gmail.com>
2020-08-30 10:42:16 -0400

    email added

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -5,10 +5,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: theme.palette.secondary.light
+    backgroundColor: theme.palette.secondary.light,
+    minHeight: "500px"
   },
-  title: {
+  text: {
     color: "white",
+    textAlign: "center"
+  },
+  link: {
+    color: theme.palette.primary.light
   }
 }))
 
@@ -17,11 +22,13 @@ const ContactMe = ({ myRef }) => {
 
   return (
     <section id="contact-me" className={`contact-me section ${classes.root}`} ref={myRef}>
-      <Typography variant="h2" className={classes.title} component="h2" gutterBottom align="center">
+      <Typography variant="h2" className={classes.text} component="h2" gutterBottom>
         Contact Me
       </Typography>
-      <Container>
-        Hi
+      <Container className={classes.text}>
+        <Typography variant="body1">
+          Email me at <a href="mailto:leizl.samano@gmail.com" className={classes.link}>leizl.samano@gmail.com</a>.
+        </Typography>
       </Container>
     </section>
   )
