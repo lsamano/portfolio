30338474ad040774966148c0d762439b46b6e051
lsamano <leizl.samano@gmail.com>
2020-09-04 11:17:51 -0400

    made blogs section responsive

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -6,10 +6,15 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: "30%",
+    margin: "10px"
+  },
+  mobile: {
+    width: "100%",
     margin: "10px"
   },
   actionContent: {
@@ -21,9 +26,11 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const StoryCard = ({ story: { title, subtitle, url, img_url } }) => {
-  const { root, actionContent, titleFont } = useStyles();
+  const { root, mobile, actionContent, titleFont } = useStyles();
+  const matches = useMediaQuery('(min-width:850px)');
+
   return (
-    <Card className={root}>
+    <Card className={matches ? root : mobile}>
       <CardActionArea className={actionContent} href={url}>
         {img_url ? <CardMedia component="img" src={img_url} alt={title} /> : null}
         <CardContent className={actionContent}>
