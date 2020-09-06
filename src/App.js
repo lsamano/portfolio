cd5e010644e49edb3aed9dd49b68faf44ae4b673
lsamano <leizl.samano@gmail.com>
2020-09-06 10:36:53 -0400

    fixed scrolling logic

--- src/App.js
+++ src/App.js
@@ -15,15 +15,13 @@ function App(props) {
   const blogEl = useRef(null);
   const contactMeEl = useRef(null);
 
-  /////
   const isScrolledIntoView = (el) => {
     const rect = el.getBoundingClientRect();
     const elemTop = rect.top;
     const elemBottom = rect.bottom;
-    const isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    const isVisible = elemTop < window.innerHeight/2 && elemBottom >= 0;
     return isVisible;
   }
-  /////
 
   useEffect(() => {
     const listener = document.addEventListener("scroll", event => {
